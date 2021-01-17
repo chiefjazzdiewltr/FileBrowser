28046bd66b5487b92a68d177d0d10499ec6b68dc
Zachary Gillions <zachary@gillions.com>
2021-01-17 20:43:01 +1300

    Add project files.

---
+++ File Browser/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace File_Browser {
+    partial class fileBrowser {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.shareMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.openMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.openNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileMenu,
+            this.openMenu,
+            this.shareMenu,
+            this.viewMenu});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileMenu
+            // 
+            this.fileMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openNew});
+            this.fileMenu.Name = "fileMenu";
+            this.fileMenu.Size = new System.Drawing.Size(37, 20);
+            this.fileMenu.Text = "File";
+            // 
+            // shareMenu
+            // 
+            this.shareMenu.Name = "shareMenu";
+            this.shareMenu.Size = new System.Drawing.Size(48, 20);
+            this.shareMenu.Text = "Share";
+            // 
+            // viewMenu
+            // 
+            this.viewMenu.Name = "viewMenu";
+            this.viewMenu.Size = new System.Drawing.Size(44, 20);
+            this.viewMenu.Text = "View";
+            // 
+            // openMenu
+            // 
+            this.openMenu.Name = "openMenu";
+            this.openMenu.Size = new System.Drawing.Size(48, 20);
+            this.openMenu.Text = "Open";
+            // 
+            // openNew
+            // 
+            this.openNew.Name = "openNew";
+            this.openNew.Size = new System.Drawing.Size(180, 22);
+            this.openNew.Text = "Open New Browser";
+            this.openNew.Click += new System.EventHandler(this.openNewBrowserToolStripMenuItem_Click);
+            // 
+            // fileBrowser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "fileBrowser";
+            this.Text = "File Browser";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.fileBrowser_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileMenu;
+        private System.Windows.Forms.ToolStripMenuItem openNew;
+        private System.Windows.Forms.ToolStripMenuItem openMenu;
+        private System.Windows.Forms.ToolStripMenuItem shareMenu;
+        private System.Windows.Forms.ToolStripMenuItem viewMenu;
+    }
+}
+
---
+++ File Browser/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace File_Browser {
+    public partial class fileBrowser : Form {
+        public fileBrowser() {
+            InitializeComponent();
+        }
+
+        private void openNewBrowserToolStripMenuItem_Click(object sender, EventArgs e) {
+            fileBrowser newBrowser = new fileBrowser();
+            newBrowser.Show();
+        }
+
+        private void fileBrowser_FormClosing(object sender, FormClosingEventArgs e) {
+            
+        }
+    }
+}
